69444a60d7d756def3eb5bed487c5931419a86ad
AlfertHonori <t18052hi@sfc.keio.ac.jp>
2018-12-25 12:44:42 +0900

    localStorageに入れました

--- final_hw.js
+++ final_hw.js
@@ -1,3 +1,19 @@
-window.onload = function idou1(){
-  
+window.onload = function (){
+  localStorage.clear();
+}
+
+function push1() {
+  var newphone = document.getElementsByName("nphone");
+  var name = " ";
+  for (var i = 0; i<newphone.length; i++) {
+    if(newphone[i].checked) {
+      name = newphone[i].value
+    }
+  }
+  localStorage.setItem("phone", name);
+}
+
+function show1() {
+  var value = localStorage.getItem("phone");
+  alert(value);
 }
